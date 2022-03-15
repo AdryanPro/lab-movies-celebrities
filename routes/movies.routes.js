35b69db78f8a7bac9890c241970a18e704bb77ab
Adryan Cadet <adryan-pro@hotmail.com>
2022-03-15 16:34:08 +0100

    iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,12 +1,23 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const mongoose = require("mongoose");
-const Movies = require("../models/movies.model");
+const Movies = require("../models/movie.model");
 
 // all your routes here
 router.get("/movies/create", (req, res) => {
   res.render("movies/new-movie.hbs");
 });
 
-//router.post(""());
+router.post("/movies/create", async (req, res) => {
+  const moviesDB = req.body;
+
+  try {
+    await Movies.create(moviesDB);
+  } catch (err) {
+    console.log("Error creating the movie", err);
+    res.render("movies/new-movie");
+  }
+  res.redirect("/");
+});
+
 module.exports = router;
